4135964fa21a8dcdda70dbc082202f886e810981
knownAsFarah <happyalattili2242000@gmail.com>
2024-06-19 12:45:27 +0300

    Fix: Reusable code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static double addition(double x, double y){
+        return x + y;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to My Calculator");
         System.out.println("Inter two numbers to add:");
         double firstNum = sc.nextDouble();
         double secondNum = sc.nextDouble();
-        System.out.println(firstNum + secondNum);
-
+        double result = addition(firstNum , secondNum);
+        System.out.println(result);
 
     }
 }
