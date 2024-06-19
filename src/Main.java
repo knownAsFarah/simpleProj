4ece6978fae69ac99b42da19014a40b4b3f90bc7
knownAsFarah <happyalattili2242000@gmail.com>
2024-06-19 13:05:36 +0300

    Feat: more operations

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to My Calculator");
-        System.out.println("Inter two numbers to add:");
+        System.out.println("Enter one of these symbols: {'+','-','*','/','%'}, Where:\n1) '+' is for the addition operation \n2) '-' is for the subtraction operation \n3) '*' is for the multiplication operation \n4) '/' is for the division operation \n5) '%' is for the modulus operation ");
+        String op = sc.nextLine();
+        System.out.println("Inter two numbers to calculate:");
         int firstNum = sc.nextInt();
         int secondNum = sc.nextInt();
         System.out.println(firstNum + secondNum);
